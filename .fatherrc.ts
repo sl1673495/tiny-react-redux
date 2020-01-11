6be76d05e32b534f9924af895468f6f50ee5f24e
ssh <949589855@qq.com>
2020-01-11 17:31:42 +0800

    docs: 打包文档

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -6,7 +6,12 @@ type FixedIBundleOptions = DeepPartial<IBundleOptions>;
 const options: FixedIBundleOptions = {
   cjs: 'rollup',
   esm: 'rollup',
-  doc: { typescript: true, title: 'tiny-react-redux使用文档' },
+  doc: { 
+    typescript: true, 
+    title: 'tiny-react-redux使用文档',
+    base: process.env.NODE_ENV === 'production' ? require('./package.json').repository : '',
+    dest: 'docs',
+ },
 };
 
 export default options;
